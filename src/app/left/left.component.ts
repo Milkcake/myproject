f4c3650f6734bb9d1405b54ef1fc7977c2fb876c
Milkcake <onesalmon@gmail.com>
2017-05-15 14:32:54 +0700

    Day 3
    Study Day 3

--- src/app/left/left.component.ts
+++ src/app/left/left.component.ts
@@ -4,16 +4,31 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-left',
   template: `
     <p>
-      left Works!
+      Firstname : <input (keyup)="onKey($event)">
+      <br>
+      Lastname : <input #lastname (keyup.enter)="onEnter(lastname.value)">
+      {{firstName}} 
+      <br>
+      {{lastName}}
     </p>
   `,
   styles: []
 })
 export class LeftComponent implements OnInit {
+  firstName:string;
+  lastName:string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onKey(event:KeyboardEvent){
+    this.firstName=((<HTMLInputElement>event.target).value);
+  }
+
+  onEnter(value:string){
+    this.lastName = value;
   }
 
 }
